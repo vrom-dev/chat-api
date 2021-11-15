f3e98fa2b2d29b63a5d9d6cd8de6330d903e5b27
Víctor Romero <vromerocomino@gmail.com>
2021-11-15 09:30:21 +0100

    Socket-io listener init

--- app/listeners/index.js
+++ app/listeners/index.js
@@ -1,23 +1,30 @@
-// https://aleemisiaka.com/blog/socketio-app-structure/
-const fs = require('fs')
-const path = require('path')
+const userJoinsChannel = require('../events/userJoinsChannel')
+const userSendsMessage = require('../events/userSendsMessage')
+const userDisconnects = require('../events/userDisconnects')
 
-const initListeners = io => {
-  const listenersPath = path.resolve(__dirname)
+const ActiveUsers = require('../models/activeUsers')
 
-  fs.readdir(listenersPath, (err, files) => {
-    if (err) {
-      process.exit(1)
-    }
+const activeUsersList = (activeUsers) => {
+  const activeUsersKeys = Object.keys(activeUsers)
+  const newActiveUsersObject = activeUsersKeys.reduce((acc, channel) => {
+    acc[channel] = activeUsers[channel].map(user => user.username)
+    return acc
+  }, {})
+  return newActiveUsersObject
+}
 
-    files.forEach(fileName => {
-      if (fileName !== 'index.js') {
-        console.debug('Initializing listener from: %s', fileName)
-        const listener = require(path.resolve(__dirname, fileName))
-        listener(io)
-      }
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    socket.on('user joins channel', ({ channel, username }) => {
+      userJoinsChannel(socket, channel, ActiveUsers, username)
+      io.emit('active users', activeUsersList(ActiveUsers))
+    })
+    socket.on('user sends message', (data) => {
+      userSendsMessage(socket, data)
+    })
+    socket.on('disconnect', () => {
+      userDisconnects(socket, ActiveUsers)
+      io.emit('active users', activeUsersList(ActiveUsers))
     })
   })
 }
-
-module.exports = initListeners
